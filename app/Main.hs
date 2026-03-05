4148cca4c874f297e576a09ed83be3e5f72047e9
Ganath-Avinash <ganathavinash.gr@gmail.com>
2026-03-05 18:05:40 +0530

    New

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,30 @@ import Types
 import EmissionCalculator
 
 main :: IO ()
-main = do
-    putStrLn "=== Transport Emissions Calculator ==="
+main = menu
 
-    putStrLn "Enter city name:"
+menu :: IO ()
+menu = do
+    putStrLn "\n=== Transport Emissions System ==="
+    putStrLn "1. Calculate emission"
+    putStrLn "2. Compare cities"
+    putStrLn "3. Load dataset"
+    putStrLn "4. Exit"
+
+    putStrLn "Enter your choice:"
+    choice <- getLine
+
+    case choice of
+        "1" -> calculateEmission >> menu
+        "2" -> compareCities >> menu
+        "3" -> loadDataset >> menu
+        "4" -> putStrLn "Exiting program..."
+        _   -> putStrLn "Invalid choice!" >> menu
+
+
+calculateEmission :: IO ()
+calculateEmission = do
+    putStrLn "\nEnter city name:"
     cityName <- getLine
 
     putStrLn "Enter population:"
@@ -18,7 +38,7 @@ main = do
     vehInput <- getLine
     let vehVal = read vehInput :: Int
 
-    putStrLn "Enter emission per vehicle:"
+    putStrLn "Enter emission per vehicle (tons CO2 per year):"
     emInput <- getLine
     let emission = read emInput :: Double
 
@@ -31,3 +51,13 @@ main = do
     putStrLn ("City: " ++ cityName)
     putStrLn ("Total Emission: " ++ show total)
     putStrLn ("Per Capita Emission: " ++ show perCap)
+
+
+compareCities :: IO ()
+compareCities = do
+    putStrLn "\nCity comparison feature coming soon!"
+
+
+loadDataset :: IO ()
+loadDataset = do
+    putStrLn "\nDataset loading feature coming soon!"
