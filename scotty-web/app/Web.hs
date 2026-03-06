49cd4d42efb22b9e483fcc356bd56918fbcfab8f
Kavin-JS <kavinjs21@gmail.com>
2026-03-06 19:01:02 +0530

    end

--- scotty-web/app/Web.hs
+++ scotty-web/app/Web.hs
@@ -3,7 +3,18 @@
 module Main where
 
 import Web.Scotty
+import qualified Data.ByteString.Lazy as BL
 
 main :: IO ()
 main = scotty 3000 $ do
-  get "/" $ file "static/index.html"
+
+  -- Serve the dashboard
+  get "/" $ do
+    file "static/index.html"
+
+  -- Serve analysis data as JSON
+  get "/analysis" $ do
+    jsonData <- liftIO $ BL.readFile "../analysis.json"
+    setHeader "Content-Type" "application/json; charset=utf-8"
+    setHeader "Access-Control-Allow-Origin" "*"
+    raw jsonData
